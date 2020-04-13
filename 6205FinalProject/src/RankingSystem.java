c0b3505ce62ecc357453a03ac99a528b65493566
HuaruiLu <55645821+HuaruiLu@users.noreply.github.com>
2020-04-13 00:17:09 -0400

    1

--- 6205FinalProject/src/RankingSystem.java
+++ 6205FinalProject/src/RankingSystem.java
@@ -1,27 +1,20 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class RankingSystem {
 
     MatchDirectory matchDirectory;
     TeamDirectory teamDirectory;
+    private static String xls = "6205FinalProject\\main\\resources\\20192020.csv";
 
     public RankingSystem(){
         this.matchDirectory = new MatchDirectory();
@@ -30,6 +23,12 @@ public class RankingSystem {
 
     public static void main(String[] args){
 
+        System.out.println("************读取Excel2003的主表数据（将Excel单元格设为文本格式）******************");
+        long start2003 = System.currentTimeMillis();
+        List<Match> list2003 = ReadExcel.readFromXLS2003(xls);
+        for (Match match : list2003) {
+            System.out.println(match);
+        }
     }
 
     public void matchHistory(){
@@ -61,20 +60,18 @@ public class RankingSystem {
 
     public static class ReadExcel {
 
-        private static String xls2003 = "D:\\employee.xls";
-        private static String xlsx2007 = "D:\\employee.xlsx";
-
         /**
          * 读取Excel2003的主表数据 （单个sheet）
+         *
          * @param filePath
          * @return
          */
-        private static List<> readFromXLS2003(String filePath) {
+        private static List<Match> readFromXLS2003(String filePath) {
             File excelFile = null;// Excel文件对象
             InputStream is = null;// 输入流对象
             String cellStr = null;// 单元格，最终按字符串处理
-            List<Employee> employeeList = new ArrayList<Employee>();// 返回封装数据的List
-            Employee employee = null;// 每一个雇员信息对象
+            List<Match> matchesList = new ArrayList<>();// 返回封装数据的List
+            Match match = null;// 每一个雇员信息对象
             try {
                 excelFile = new File(filePath);
                 is = new FileInputStream(excelFile);// 获取文件输入流
@@ -83,7 +80,7 @@ public class RankingSystem {
                 // 开始循环遍历行，表头不处理，从1开始
                 for (int i = 1; i <= sheet.getLastRowNum(); i++) {
                     HSSFRow row = sheet.getRow(i);// 获取行对象
-                    employee = new Employee();// 实例化Student对象
+                    Match match1 = new Match();// 实例化Student对象
                     if (row == null) {// 如果为空，不处理
                         continue;
                     }
@@ -92,29 +89,29 @@ public class RankingSystem {
                         HSSFCell cell = row.getCell(j);// 获取单元格对象
                         if (cell == null) {// 单元格为空设置cellStr为空串
                             cellStr = "";
-                        } else if (cell.getCellType() == HSSFCell.CELL_TYPE_BOOLEAN) {// 对布尔值的处理
-                            cellStr = String.valueOf(cell.getBooleanCellValue());
-                        } else if (cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {// 对数字值的处理
-                            cellStr = cell.getNumericCellValue() + "";
+//                        } else if (cell.getCellType() == HSSFCell.CELL_TYPE_BOOLEAN) {// 对布尔值的处理
+//                            cellStr = String.valueOf(cell.getBooleanCellValue());
+//                        } else if (cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {// 对数字值的处理
+//                            cellStr = cell.getNumericCellValue() + "";
                         } else {// 其余按照字符串处理
                             cellStr = cell.getStringCellValue();
                         }
                         // 下面按照数据出现位置封装到bean中
-                        if (j == 0) {
-                            employee.setName(cellStr);
-                        } else if (j == 1) {
-                            employee.setGender(cellStr);
-                        } else if (j == 2) {
-                            employee.setAge(new Double(cellStr).intValue());
+                        if (j == 2) {
+                            match1.setTeamA(cellStr);
                         } else if (j == 3) {
-                            employee.setDepartment(cellStr);
-                        } else if(j == 4){
-                            employee.setSalary(new Double(cellStr).intValue());
-                        }else {
-                            employee.setDate(cellStr);
+                            match1.setTeamB(cellStr);
+//                        } else if (j == 2) {
+//                            employee.setAge(new Double(cellStr).intValue());
+//                        } else if (j == 3) {
+//                            employee.setDepartment(cellStr);
+//                        } else if(j == 4){
+//                            employee.setSalary(new Double(cellStr).intValue());
+//                        }else {
+//                            employee.setDate(cellStr);
                         }
                     }
-                    employeeList.add(employee);// 数据装入List
+                    matchesList.add(match1);// 数据装入List
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -127,8 +124,8 @@ public class RankingSystem {
                     }
                 }
             }
-            return employeeList;
+            return matchesList;
         }
 
-
+    }
 }
